3c267a8911adf8b08cab46629c283f092d90da09
Casper <casperfj2011@gmail.com>
2022-05-07 21:17:32 +0200

    Added OSGILab

---
+++ Exercises/OSGILab/AsteroidsEntityFramework/dk.sdu.mmmi.cbse.core/src/main/java/dk/sdu/mmmi/cbse/core/GameCore.java
@@ -0,0 +1,137 @@
+package dk.sdu.mmmi.cbse.core;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+import dk.sdu.mmmi.cbse.core.managers.GameInputProcessor;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class GameCore extends Game {
+
+    private static OrthographicCamera cam;
+    private ShapeRenderer sr;
+
+    private LwjglApplication application;
+    private GameData gameData;
+    private World world = new World();
+
+    private static List<IEntityProcessingService> entityProcessorList = new CopyOnWriteArrayList<>();
+    private static List<IGamePluginService> gamePluginList = new CopyOnWriteArrayList<>();
+    private static List<IPostEntityProcessingService> postEntityProcessorList = new CopyOnWriteArrayList<>();
+
+    public GameCore() {
+        this.gameData = new GameData();
+
+        this.init();
+    }
+
+    private void init() {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Asteroids";
+        cfg.width = 500;
+        cfg.height = 400;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
+        this.application = new LwjglApplication(this, cfg);
+    }
+
+    public void create() {
+        gameData.setDisplayWidth(Gdx.graphics.getWidth());
+        gameData.setDisplayHeight(Gdx.graphics.getHeight());
+
+        cam = new OrthographicCamera(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        cam.translate(gameData.getDisplayWidth() / 2, gameData.getDisplayHeight() / 2);
+        cam.update();
+
+        sr = new ShapeRenderer();
+
+        Gdx.input.setInputProcessor(
+                new GameInputProcessor(gameData)
+        );
+    }
+
+    @Override
+    public void render() {
+        // clear screen to black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        gameData.setDelta(Gdx.graphics.getDeltaTime());
+        gameData.getKeys().update();
+
+        this.update();
+        this.draw();
+    }
+
+    private void update() {
+        // Update
+        for (IEntityProcessingService entityProcessorService : entityProcessorList) {
+            entityProcessorService.process(gameData, world);
+        }
+
+        // Post Update
+        for (IPostEntityProcessingService postEntityProcessorService : postEntityProcessorList) {
+            postEntityProcessorService.process(gameData, world);
+        }
+    }
+
+    private void draw() {
+        for (Entity entity : world.getEntities()) {
+            sr.setColor(1, 1, 1, 1);
+
+            sr.begin(ShapeRenderer.ShapeType.Line);
+
+            float[] shapex = entity.getShapeX();
+            float[] shapey = entity.getShapeY();
+
+            for (int i = 0, j = shapex.length - 1;
+                 i < shapex.length;
+                 j = i++) {
+
+                sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
+            }
+
+            sr.end();
+        }
+    }
+
+    public void addEntityProcessingService(IEntityProcessingService eps) {
+        this.entityProcessorList.add(eps);
+    }
+
+    public void removeEntityProcessingService(IEntityProcessingService eps) {
+        this.entityProcessorList.remove(eps);
+    }
+
+    public void addPostEntityProcessingService(IPostEntityProcessingService eps) {
+        postEntityProcessorList.add(eps);
+    }
+
+    public void removePostEntityProcessingService(IPostEntityProcessingService eps) {
+        postEntityProcessorList.remove(eps);
+    }
+
+    public void addGamePluginService(IGamePluginService plugin) {
+        this.gamePluginList.add(plugin);
+        plugin.start(gameData, world);
+
+    }
+
+    public void removeGamePluginService(IGamePluginService plugin) {
+        this.gamePluginList.remove(plugin);
+        plugin.stop(gameData, world);
+    }
+}
